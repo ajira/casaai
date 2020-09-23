060dfc03b94de202083136eb7490afce58df3043
Arun Madhavan <arun@ajira.tech>
2020-09-24 01:28:20 +0530

    Arun | New Changes

---
+++ precache-manifest.21cc8594ed1359f9cf6b08fd04084ae3.js
@@ -0,0 +1,430 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cd919161586d1df35427cb77adcb79",
+    "url": "/casaai/index.html"
+  },
+  {
+    "revision": "7f44e63e53f236379d78",
+    "url": "/casaai/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "dbd4e2154200d416da86",
+    "url": "/casaai/static/css/main.1dcc8418.chunk.css"
+  },
+  {
+    "revision": "7f44e63e53f236379d78",
+    "url": "/casaai/static/js/2.f14b6abc.chunk.js"
+  },
+  {
+    "revision": "a3b889260cce3a3f17b6182b829b261f",
+    "url": "/casaai/static/js/2.f14b6abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd4e2154200d416da86",
+    "url": "/casaai/static/js/main.c19b1d11.chunk.js"
+  },
+  {
+    "revision": "e6c553cae4b96cc731cf",
+    "url": "/casaai/static/js/runtime-main.3e9ba161.js"
+  },
+  {
+    "revision": "41f205d0f031cabe72bd6a1dd0cc9047",
+    "url": "/casaai/static/media/0.41f205d0.png"
+  },
+  {
+    "revision": "b25afa7a56ee528757130098b31ab192",
+    "url": "/casaai/static/media/60882360.b25afa7a.png"
+  },
+  {
+    "revision": "2e9d4b215880226df3e27b80c11cc590",
+    "url": "/casaai/static/media/Group 2277.2e9d4b21.svg"
+  },
+  {
+    "revision": "292f1d2bb2aaf77bc7a5c2ef6d9b18d2",
+    "url": "/casaai/static/media/Group 2278.292f1d2b.svg"
+  },
+  {
+    "revision": "040b155a5c5bda994b9fd51996b05d49",
+    "url": "/casaai/static/media/Group 2279.040b155a.svg"
+  },
+  {
+    "revision": "59a44925fc9260dde872043b4e9de197",
+    "url": "/casaai/static/media/Group 2280.59a44925.svg"
+  },
+  {
+    "revision": "6a096999f6da1823f0ee30cc26a76ac8",
+    "url": "/casaai/static/media/Group 2281.6a096999.svg"
+  },
+  {
+    "revision": "a82e022f1e6268ab2f709e139ca41c03",
+    "url": "/casaai/static/media/Group 2282.a82e022f.svg"
+  },
+  {
+    "revision": "281009a6a1a43ec625c86a951202b34c",
+    "url": "/casaai/static/media/Group 2283.281009a6.svg"
+  },
+  {
+    "revision": "97fc0f81a44a2a24b453db810df8606c",
+    "url": "/casaai/static/media/Group 2284.97fc0f81.svg"
+  },
+  {
+    "revision": "c600eb9eda53fea789ce9a26924b5f21",
+    "url": "/casaai/static/media/Group 2285.c600eb9e.svg"
+  },
+  {
+    "revision": "3e7421e085b60b4aec3507126034da4d",
+    "url": "/casaai/static/media/Group 2715.3e7421e0.svg"
+  },
+  {
+    "revision": "4a34223add93442cc304e64c9c689a4f",
+    "url": "/casaai/static/media/Group 2820.4a34223a.svg"
+  },
+  {
+    "revision": "2b44e1530fc289498646831ee0680f33",
+    "url": "/casaai/static/media/Group 2980.2b44e153.png"
+  },
+  {
+    "revision": "782a5496981dea9c521d42e9a4706f65",
+    "url": "/casaai/static/media/IMG_20190803_083559.782a5496.png"
+  },
+  {
+    "revision": "91d5ca00d8a1557ee527343f5a048e92",
+    "url": "/casaai/static/media/Image 1.91d5ca00.png"
+  },
+  {
+    "revision": "41b307b4b4afb4a3844cf9cfdaef25ad",
+    "url": "/casaai/static/media/Image 50@2x.41b307b4.png"
+  },
+  {
+    "revision": "50f087210ca5b9882651572f9e15865b",
+    "url": "/casaai/static/media/Image 51@2x.50f08721.png"
+  },
+  {
+    "revision": "aced1cbf5f0b962d49277df5ac5f5ba0",
+    "url": "/casaai/static/media/Image 61.aced1cbf.png"
+  },
+  {
+    "revision": "671ad05d8cf881d9138ec15020493223",
+    "url": "/casaai/static/media/Image 74.671ad05d.png"
+  },
+  {
+    "revision": "cc797627fdfa18803c479a7a60a9044d",
+    "url": "/casaai/static/media/Image 77.cc797627.png"
+  },
+  {
+    "revision": "730566c357fe97da65951193375a76ae",
+    "url": "/casaai/static/media/Image 78.730566c3.png"
+  },
+  {
+    "revision": "1829dc4b2680ff61683791b12c2b150d",
+    "url": "/casaai/static/media/Image 79.1829dc4b.png"
+  },
+  {
+    "revision": "0493f7f9bbaabab67e29bbc037b41083",
+    "url": "/casaai/static/media/Image 80.0493f7f9.png"
+  },
+  {
+    "revision": "51e4b8f3605de8ca7e463ab44831be20",
+    "url": "/casaai/static/media/Image 82.51e4b8f3.png"
+  },
+  {
+    "revision": "bd95668f4a110eb0eb68f993db81b980",
+    "url": "/casaai/static/media/Image 83.bd95668f.png"
+  },
+  {
+    "revision": "267e7ec99e2c24b80a6ff0335b12381e",
+    "url": "/casaai/static/media/Image 84.267e7ec9.png"
+  },
+  {
+    "revision": "19fd7e06631926523915e0ad04f14085",
+    "url": "/casaai/static/media/Image 85.19fd7e06.png"
+  },
+  {
+    "revision": "ed7bfe3091ca55bbe0cd84627c971d34",
+    "url": "/casaai/static/media/Image 86.ed7bfe30.png"
+  },
+  {
+    "revision": "3f91c4b698a89ae7f57042b380d00583",
+    "url": "/casaai/static/media/Image 88.3f91c4b6.png"
+  },
+  {
+    "revision": "8a70826d056ee3ff16123c43f9e15602",
+    "url": "/casaai/static/media/Image 89.8a70826d.png"
+  },
+  {
+    "revision": "b480d095d90f958b5797b5b5c51e789f",
+    "url": "/casaai/static/media/Image 90.b480d095.png"
+  },
+  {
+    "revision": "571f0e272b68ab48033c30f83e1a05a5",
+    "url": "/casaai/static/media/Image 91.571f0e27.png"
+  },
+  {
+    "revision": "873243f5c412cb8ce350306786e23a9c",
+    "url": "/casaai/static/media/Image 92.873243f5.png"
+  },
+  {
+    "revision": "ea79dc68b28641a095edfe0a59cc6fce",
+    "url": "/casaai/static/media/Image 93.ea79dc68.png"
+  },
+  {
+    "revision": "20ab06c465f40ea1c7f5403104ada646",
+    "url": "/casaai/static/media/Image 94.20ab06c4.png"
+  },
+  {
+    "revision": "3d1a49efb3bb0b4a1c188736d3b0a3b2",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.43.53_AM-removebg copy 2.3d1a49ef.png"
+  },
+  {
+    "revision": "565875a1f1c5956c5842a58f7edfcff0",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.43.53_AM-removebg copy 3.565875a1.png"
+  },
+  {
+    "revision": "bf5ccb3d2fc455e7a0799061338cd830",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.43.53_AM-removebg copy 4.bf5ccb3d.png"
+  },
+  {
+    "revision": "538b957a68d9486b581b6f316fe2193a",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.43.53_AM-removebg copy 5.538b957a.png"
+  },
+  {
+    "revision": "06b887dd6b6f5a62e25a4a1c20d88bd5",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.43.53_AM-removebg copy.06b887dd.png"
+  },
+  {
+    "revision": "5134deed005ea037ec796126b1400a0a",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.56.44_AM-removebg-preview (1).5134deed.png"
+  },
+  {
+    "revision": "dc7ac0c10b62d86cd4c5a76d613a9464",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.56.44_AM-removebg-preview (2).dc7ac0c1.png"
+  },
+  {
+    "revision": "4ed4e41ed3dba43dacf46f648d0ac237",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.56.44_AM-removebg-preview (3).4ed4e41e.png"
+  },
+  {
+    "revision": "6ebbfe98ed6e1edf840ce97949c04530",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.56.44_AM-removebg-preview (4).6ebbfe98.png"
+  },
+  {
+    "revision": "2b32a2e1521309c3609fddfe4d5e2335",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_11.56.44_AM-removebg-preview.2b32a2e1.png"
+  },
+  {
+    "revision": "71dc32562953e56ab80bbe8df57d3ba4",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.52.21_PM-removebg-preview (1).71dc3256.png"
+  },
+  {
+    "revision": "d6f4cdb5bc253ef1da2fcccb3c0d061f",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.52.21_PM-removebg-preview (2).d6f4cdb5.png"
+  },
+  {
+    "revision": "fa989cc8a03c6baccea9c5f6f70bee78",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.52.21_PM-removebg-preview (4).fa989cc8.png"
+  },
+  {
+    "revision": "4449cfe22d17c6f6d2a7d5dc3b1269ea",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.52.21_PM-removebg-preview (5).4449cfe2.png"
+  },
+  {
+    "revision": "e1f91d46e95a0c74cb429a3ab3a01e5a",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.52.21_PM-removebg-preview.e1f91d46.png"
+  },
+  {
+    "revision": "48021ed6c4a5f277a2216c1bbc7ae0df",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.57.39_PM-removebg-preview (1).48021ed6.png"
+  },
+  {
+    "revision": "7218c8436096dcbeb9374b5dc38c2f96",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.57.39_PM-removebg-preview (2).7218c843.png"
+  },
+  {
+    "revision": "9134f45e69b5a68d5bfb9d595897a3ef",
+    "url": "/casaai/static/media/Screenshot_2020-09-23_at_3.57.39_PM-removebg-preview.9134f45e.png"
+  },
+  {
+    "revision": "75d73327db454e900da2249babcf591c",
+    "url": "/casaai/static/media/achievement__monochromatic (1).75d73327.svg"
+  },
+  {
+    "revision": "f08589f8fd5199a67c0ba03c9670f00c",
+    "url": "/casaai/static/media/analytics_process_monochromatic.f08589f8.svg"
+  },
+  {
+    "revision": "cfec224e7fbf4c7cc60cb57bbefc413b",
+    "url": "/casaai/static/media/bank_note_monochromatic.cfec224e.svg"
+  },
+  {
+    "revision": "8677652b6870979c37755aa893d81920",
+    "url": "/casaai/static/media/career__monochromatic (1).8677652b.svg"
+  },
+  {
+    "revision": "467c2d8340a035598acc1e1ed094da57",
+    "url": "/casaai/static/media/content_creation (1).467c2d83.svg"
+  },
+  {
+    "revision": "a922f946b81ae8e82654bbc1f1aa048c",
+    "url": "/casaai/static/media/creative_process_monochromatic.a922f946.svg"
+  },
+  {
+    "revision": "429fd9c7143b32d31c80a115a3fb0aa8",
+    "url": "/casaai/static/media/credit_card_monochromatic.429fd9c7.svg"
+  },
+  {
+    "revision": "03f4af68146c3b12f00477c76621204d",
+    "url": "/casaai/static/media/data_analytics_.03f4af68.svg"
+  },
+  {
+    "revision": "21d1d3f0da9629dcb03ce6dc8bb98138",
+    "url": "/casaai/static/media/data_analytics__monochromatic (1).21d1d3f0.svg"
+  },
+  {
+    "revision": "472b12a2f3dde604ecf007f1ba2a9d65",
+    "url": "/casaai/static/media/data_storage.472b12a2.svg"
+  },
+  {
+    "revision": "d6629ba33e9707f4992b32ae0ce49aad",
+    "url": "/casaai/static/media/email_campaign.d6629ba3.svg"
+  },
+  {
+    "revision": "2b54e55250aca208a33391964cffccc1",
+    "url": "/casaai/static/media/fast_working.2b54e552.svg"
+  },
+  {
+    "revision": "069ee29637dde4be8218185d339b785e",
+    "url": "/casaai/static/media/finance_analytics__monochromatic (1).069ee296.svg"
+  },
+  {
+    "revision": "bb053f22779e08f547e45bc132d97882",
+    "url": "/casaai/static/media/financial_analyst_monochromatic.bb053f22.svg"
+  },
+  {
+    "revision": "6b28d2d58eaa872de912ce48fac9dbed",
+    "url": "/casaai/static/media/icons8-business-building-100.6b28d2d5.png"
+  },
+  {
+    "revision": "d79985d97190d4b0c1b1109bea2dcfa6",
+    "url": "/casaai/static/media/icons8-city-buildings-100.d79985d9.png"
+  },
+  {
+    "revision": "6820f9b1a7809dde5445dd5d509baf7f",
+    "url": "/casaai/static/media/icons8-find-user-male-100.6820f9b1.png"
+  },
+  {
+    "revision": "b80ba953b171d4c9a22a0c8b1de10312",
+    "url": "/casaai/static/media/icons8-small-business-100.b80ba953.png"
+  },
+  {
+    "revision": "9d14e171d8dd6afd9b6b6693b19cc4c1",
+    "url": "/casaai/static/media/information_flow_monochromatic.9d14e171.svg"
+  },
+  {
+    "revision": "50f7cb0881e589464842b85900708384",
+    "url": "/casaai/static/media/marketing_monochromatic.50f7cb08.svg"
+  },
+  {
+    "revision": "2e3c7c8b5e32e5100fcfdbc986586ee2",
+    "url": "/casaai/static/media/new_campaign_monochromatic.2e3c7c8b.svg"
+  },
+  {
+    "revision": "b126d6a4f664282357c4119e2fb31b43",
+    "url": "/casaai/static/media/online_payment_monochromatic.b126d6a4.svg"
+  },
+  {
+    "revision": "2ad58cf185cdc4f575ff6dc265916e29",
+    "url": "/casaai/static/media/online_store__monochromatic (1).2ad58cf1.svg"
+  },
+  {
+    "revision": "b43fa697e71f4e9a162f92398f52013b",
+    "url": "/casaai/static/media/order_confirmation_monochromatic.b43fa697.svg"
+  },
+  {
+    "revision": "de2412d19c36081c89133b4fa2773b43",
+    "url": "/casaai/static/media/party_monochromatic (1).de2412d1.svg"
+  },
+  {
+    "revision": "d59eb89961703a92259dbb0668e0ce8b",
+    "url": "/casaai/static/media/personal_data_.d59eb899.svg"
+  },
+  {
+    "revision": "6d37e1c9f9271f26b89eafdd485b679a",
+    "url": "/casaai/static/media/pexels-andrea-piacquadio-842567-1.6d37e1c9.png"
+  },
+  {
+    "revision": "9d9c2180456209b95db2359c5d5dd8b1",
+    "url": "/casaai/static/media/pexels-andrea-piacquadio-842567.9d9c2180.png"
+  },
+  {
+    "revision": "5bb38c238f66624db9cd3a48b785e3a6",
+    "url": "/casaai/static/media/pr_monochromatic (1).5bb38c23.svg"
+  },
+  {
+    "revision": "284f32e40ae9075bd077c67d4279ee49",
+    "url": "/casaai/static/media/profiling_monochromatic.284f32e4.svg"
+  },
+  {
+    "revision": "23d4d9baf01bd9f74d2d1f376b92e147",
+    "url": "/casaai/static/media/report_analysis__monochromatic (1).23d4d9ba.svg"
+  },
+  {
+    "revision": "c3596b6595a2e53fedfd1f9e16352f6a",
+    "url": "/casaai/static/media/rsz_1rsz_vivo-2.c3596b65.png"
+  },
+  {
+    "revision": "7d1787fc1a4ba98fcb2a308c4087a530",
+    "url": "/casaai/static/media/shopping_monochromatic (1).7d1787fc.svg"
+  },
+  {
+    "revision": "c25fb5ec5d95440f18cbad6c39651622",
+    "url": "/casaai/static/media/social_media_monochromatic.c25fb5ec.svg"
+  },
+  {
+    "revision": "b0be70bf6721ebfe4d1125ee0d583c74",
+    "url": "/casaai/static/media/spotlight__monochromatic.b0be70bf.svg"
+  },
+  {
+    "revision": "f8d1b684dc572944879d4d9d921e5371",
+    "url": "/casaai/static/media/target_audience__monochromatic (1).f8d1b684.svg"
+  },
+  {
+    "revision": "ccdd00f6b7b611880ef51ca74627fa97",
+    "url": "/casaai/static/media/undraw_Artificial_intelligence_re_enpp.ccdd00f6.svg"
+  },
+  {
+    "revision": "491749162cd7a12ba750e9dcbb5d8c66",
+    "url": "/casaai/static/media/undraw_Books_l33t.49174916.svg"
+  },
+  {
+    "revision": "ffba2a9f4c92a4bec759e950b211ff0c",
+    "url": "/casaai/static/media/undraw_buffer_wq43.ffba2a9f.svg"
+  },
+  {
+    "revision": "3eb687b78169ade0024cb735da58a750",
+    "url": "/casaai/static/media/undraw_celebration_0jvk.3eb687b7.png"
+  },
+  {
+    "revision": "582d7842e82805cf6f39ff71498cf7be",
+    "url": "/casaai/static/media/undraw_gift_card_6ekc.582d7842.svg"
+  },
+  {
+    "revision": "763817f877be4fd9dc22389defcf05d9",
+    "url": "/casaai/static/media/undraw_online_wishes_dlmr.763817f8.svg"
+  },
+  {
+    "revision": "0c40b8e0117ab2eb44859828ff9f4c79",
+    "url": "/casaai/static/media/undraw_predictive_analytics_kf9n (4).0c40b8e0.svg"
+  },
+  {
+    "revision": "7d566447a83cf1ff5930654aedd06a7e",
+    "url": "/casaai/static/media/undraw_social_dashboard_k3pt.7d566447.svg"
+  },
+  {
+    "revision": "b55257c411b8fdc43e7f3980fc683bbe",
+    "url": "/casaai/static/media/undraw_successful_purchase_uyin.b55257c4.svg"
+  },
+  {
+    "revision": "0162d567000821dc8104816bb20786a4",
+    "url": "/casaai/static/media/undraw_web_shopping_dd4l.0162d567.svg"
+  }
+]);
